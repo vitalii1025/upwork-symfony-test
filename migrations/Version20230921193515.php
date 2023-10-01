4bd6d710d41bb862792c6c1fb35781ce1fe9b931
vitalii-php-dev <pvitalii818@gmail.com>
2023-10-01 06:49:22 -0500

    add: product table in migration

--- migrations/Version20230921193515.php
+++ migrations/Version20230921193515.php
@@ -21,11 +21,13 @@ final class Version20230921193515 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE tax (id INT AUTO_INCREMENT NOT NULL, country_name VARCHAR(255) NOT NULL, tax_percent NUMERIC(5, 2) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, price NUMERIC(5, 2) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE tax');
+        $this->addSql('DROP TABLE product');
     }
 }
